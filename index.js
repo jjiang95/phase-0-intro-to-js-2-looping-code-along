790038bbbb76decc04caa093974ddd89845c917a
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-03-05 08:41:17 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(names, occasion) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return messages;
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
